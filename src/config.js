43e971d4b9cccd6afc2518f51d59e0dedf5ad7b7
Oliva96 <oliva961206@gmail.com>
2022-03-01 16:58:29 -0500

    generator DONE

---
+++ src/config.js
@@ -0,0 +1,101 @@
+// General metadata for Ethereum
+const namePrefix = "Miami Frenchie Club Collection";
+const description = "Remember to replace this description";
+const baseUri = "ipfs://NewIPFS_URI";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 10,
+    layersOrder: [
+      { name: "Backgrounds" },
+      { name: "Body" },
+      { name: "Clothes" },
+      { name: "Eyes" },
+      { name: "Jewerly" },
+      { name: "Mouth" },
+      { name: "Tops" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 2000,
+  height: 2000,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: false,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  gif,
+  preview_gif,
+};
